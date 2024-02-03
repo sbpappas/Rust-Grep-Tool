32b374848f7f66cee836c3fda73fc7fa39716762
Samuel <Samuel@Samuels-MacBook-2.local>
2024-02-03 12:56:17 -0600

    updated to iterators

--- src/lib.rs
+++ src/lib.rs
@@ -18,24 +18,36 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-impl Config {
+/*impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
+        }*/
+        impl Config {
+            pub fn build(
+                mut args: impl Iterator<Item = String>,
+            ) -> Result<Config, &'static str> {
+                args.next();
+        
+                let query = match args.next() {
+                    Some(arg) => arg,
+                    None => return Err("Didn't get a query string"),
+                };
+        
+                let file_path = match args.next() {
+                    Some(arg) => arg,
+                    None => return Err("Didn't get a file path"),
+                };
+        
+                let ignore_case = env::var("IGNORE_CASE").is_ok();
+        
+                Ok(Config {
+                    query,
+                    file_path,
+                    ignore_case,
+                })
+            }
         }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
-    }
-}
 
 pub struct Config {
     pub query: String,
@@ -44,13 +56,10 @@ pub struct Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }   
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ use minigrep::Config;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -17,7 +15,7 @@ fn main() {
     println!(" in file {}: ", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
